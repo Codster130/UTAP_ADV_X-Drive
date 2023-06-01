57a468e8b37c3d1936372e8ee967b0271e72758c
Codster130 <cjr130norman@hotmail.com>
2023-06-01 15:54:03 -0400

    Main Commit

---
+++ launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/pi/Desktop/RPi
+python3 UTAP_2022_H-Drive.py
+cd /
